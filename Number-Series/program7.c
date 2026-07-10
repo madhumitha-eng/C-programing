7c6bf59b715dd1af593e8e76c9dc37cfea32bb0a
madhumitha-eng <madhumithaanjel@gmail.com>
2026-07-10 13:51:24 +0530

    To find the greater of two numbers

---
+++ Number-Series/program7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+scanf("%d",&a);
+scanf("%d",&b);
+if(a>b)
+  printf(" Greater number is: %d",a)
+    else
+  printf(" Greater number is: %d",b);
+return 0;
+}
