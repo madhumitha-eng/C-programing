15b1ce120d8270110418297b549b38055855d4d8
madhumitha-eng <madhumithaanjel@gmail.com>
2026-07-11 10:50:45 +0530

    Implement salary calculation with bonuses
    Fix variable names and calculation of total salary.

---
+++ Number-Series/program17.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int salary,bonus=1000,totalsalary;
+float hra=0.20,da=0.10;
+scanf("%d",&slary);
+totalsalary=hra+da+salary;
+if(totalsalary>50000){
+  totalsalary=totalsalary+5000;
+printf("gross slaray=%d",total salary);
+}
+else
+  ptintf("Gross salary=%d",total slary);
+return 0;
+}
