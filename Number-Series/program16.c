1002d3abe0a3d3a2643b196045634d56ef4d3c5e
madhumitha-eng <madhumithaanjel@gmail.com>
2026-07-11 10:37:01 +0530

    Implement bill calculation in program16.c
    Fix calculation of total bill and correct return statement.

---
+++ Number-Series/program16.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int unit,bill;
+scanf("%d",&unit);
+bill=unit*8;
+if(bill>=1000)
+  total=bill*0.10;
+  printf("Final Bill=%d",total);
+else
+printf("final Bill=%d",bill);
+retun 0;
+}
